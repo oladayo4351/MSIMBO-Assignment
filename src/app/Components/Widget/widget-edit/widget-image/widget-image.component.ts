eebb2e1dc0f38dd9ba756116c23595c1312ce24d
itsdayo <oladayogn@gmail.com>
2020-09-07 20:55:06 -0700

    image file upload fixed

--- src/app/Components/Widget/widget-edit/widget-image/widget-image.component.ts
+++ src/app/Components/Widget/widget-edit/widget-image/widget-image.component.ts
@@ -1,82 +1,215 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router'
-import { WidgetService } from '../../../../services/widget.service.client'
-import { Widget } from '../../../../models/widget.model.client'
-import { NgForm } from '@angular/forms'
-import { environment } from '../../../../../environments/environment'
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { WidgetService } from "../../../../services/widget.service.client";
+import { Widget } from "../../../../models/widget.model.client";
+import { NgForm } from "@angular/forms";
+import { environment } from "../../../../../environments/environment";
+import { Http, Response, Headers, RequestOptions } from "@angular/http";
+import { HttpClient } from "@angular/common/http";
+import * as AWS from "aws-sdk/global";
+import * as S3 from "aws-sdk/clients/s3";
 
+//require("aws-sdk/dist/aws-sdk");
 @Component({
-  selector: 'app-widget-image',
-  templateUrl: './widget-image.component.html',
-  styleUrls: ['./widget-image.component.css']
+  selector: "app-widget-image",
+  templateUrl: "./widget-image.component.html",
+  styleUrls: ["./widget-image.component.css"],
 })
 export class WidgetImageComponent implements OnInit {
-uid:string;
-wid: string;
-pid:string;
-wgid: string;
-_id: string;
-widgetType: string;
-pageId: string;
-size?: number;
-text?: string;
-width?: string;
-url?: string;
-name?: string;
+  uid: string;
+  wid: string;
+  pid: string;
+  wgid: string;
+  _id: string;
+  widgetType: string;
+  pageId: string;
+  size?: number;
+  text?: string;
+  width?: string;
+  url?: string;
+  name?: string;
+  Name: string;
+  myFile: File;
+  baseUrl: string;
+  fileUrl: string;
 
-baseUrl: string;
+  widget: Widget = {
+    widgetType: "",
+    pageId: "",
+  };
 
- widget: Widget ={
- 	widgetType:'',
- 	pageId:''
- };
- 
- @ViewChild('f') widgetForm: NgForm
+  @ViewChild("f") widgetForm: NgForm;
 
-
- constructor(private activatedRoute: ActivatedRoute, private widgetService: WidgetService, private router: Router) { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private widgetService: WidgetService,
+    private router: Router,
+    private http: HttpClient
+  ) {}
 
   ngOnInit() {
-  	this.activatedRoute.params.subscribe(params =>{
-  		this.uid = params['uid']
-  		this.wid = params['wid']
-  		this.pid = params['pid']
-  		this.wgid = params['wgid'] 
-  		this.baseUrl = environment.baseUrl;
-  		this.widgetService.findWidgetById(this.wgid).subscribe(
-  			(widget:Widget)=>{
-  				this.widget = widget
-  			})
-  		
-  	})
+    this.activatedRoute.params.subscribe((params) => {
+      this.uid = params["uid"];
+      this.wid = params["wid"];
+      this.pid = params["pid"];
+      this.wgid = params["wgid"];
+      this.baseUrl = environment.baseUrl;
+      this.widgetService
+        .findWidgetById(this.wgid)
+        .subscribe((widget: Widget) => {
+          this.widget = widget;
+        });
+    });
+  }
+  fileChange(files: any) {
+    let file = files.target.files[0];
+
+    const contentType = file.type;
+    const bucket = new S3({
+      accessKeyId: "AKIASF6DDRF634TMW4P3",
+      secretAccessKey: "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",
+      region: "us-west-1",
+    });
+    const params = {
+      Bucket: "web-maker",
+      Key: file.name,
+      Body: file,
+      ACL: "public-read",
+      ContentType: contentType,
+    };
+    var widgetService = this.widgetService.updateWidget.toString();
+
+    bucket.upload(params, function (err, data) {
+      if (err) {
+        console.log("There was an error uploading your file: ", err);
+        return false;
+      }
+      console.log("Successfully uploaded file.", data);
+      this.fileUrl = data.Location;
+
+      return true;
+    });
+    //for upload progress
+    // bucket
+    //   .upload(params)
+    //   .on("httpUploadProgress", function (evt) {
+    //     console.log(evt.loaded + " of " + evt.total + " Bytes");
+    //   })
+    //   .send(function (err, data) {
+    //     if (err) {
+    //       console.log("There was an error uploading your file: ", err);
+    //       return false;
+    //     }
+    //     console.log("Successfully uploaded file.", data);
+    //     this.fileUrl = data.Location;
+    //     console.log(this.fileUrl);
+    //     return true;
+    //   });
   }
 
-update(){
-	this.name = this.widgetForm.value.name
-	this.url = this.widgetForm.value.url
-	this.text = this.widgetForm.value.text
-	this.width = this.widgetForm.value.width
-	const updateWidget: Widget = {
-		 _id: this.wgid,
-		widgetType: this.widget.widgetType,
-		pageId: this.pid,
-		url: this.url,
-		width:this.width,
-		text: this.text,
-		name: this.name
-	}
+  update(e) {
+    let file = e.target[5].files[0];
 
-this.widgetService.updateWidget(this.wgid, updateWidget).subscribe(
-	(widget:Widget)=>{
-this.router.navigate(['/user', this.uid,'website',this.wid,'page',this.pid,'widget'])
-})
-}
+    const contentType = file.type;
+    const bucket = new S3({
+      accessKeyId: "AKIASF6DDRF634TMW4P3",
+      secretAccessKey: "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",
+      region: "us-west-1",
+    });
+    const params = {
+      Bucket: "web-maker",
+      Key: file.name,
+      Body: file,
+      ACL: "public-read",
+      ContentType: contentType,
+      widgetService: this.widgetService,
+      widgetForm: this.widgetForm.value,
+      widgetType: this.widget.widgetType,
+      uid: this.uid,
+      wid: this.wid,
+      wgid: this.wgid,
+      pid: this.pid,
+      pageId: this.pageId,
+      newSubmit: this.newSubmit,
+      router: this.router,
+    };
 
+    // let widgetService = this.widgetService.updateWidget();
+    // let widgetForm = this.widgetForm.value;
+    var widgetService = this.widgetService.updateWidget.toString();
 
-delete(){
-	this.widgetService.deleteWidget(this.wgid).subscribe(
-		(widgets:Widget[])=>{
-	this.router.navigate(['/user', this.uid,'website',this.wid,'page',this.pid,'widget'])
-})
-}
+    bucket.upload(params, function (err, data) {
+      if (err) {
+        console.log("There was an error uploading your file: ", err);
+        return false;
+      }
+      //  console.log("Successfully uploaded file.", data);
+
+      var fileUrl = data.Location;
+
+      this.name = params.widgetForm.name;
+      //this.url = this.widgetForm.value.url;
+      this.url = this.fileUrl;
+      this.text = params.widgetForm.text;
+      this.width = params.widgetForm.width;
+      this.widgetType = params.widgetType;
+      const updateWidget: Widget = {
+        widgetType: this.widgetType,
+        pageId: params.pageId,
+        url: fileUrl,
+        width: this.width,
+        text: this.text,
+        name: this.name,
+      };
+
+      params.newSubmit(params);
+
+      params.widgetService
+        .updateWidget(params.wgid, updateWidget)
+        .subscribe((widget: Widget) => {});
+      return true;
+    });
+  }
+
+  newSubmit(params) {
+    const updateWidget: Widget = {
+      _id: params.wgid,
+      widgetType: params.widgetType,
+      pageId: params.pid,
+
+      width: "",
+      text: "",
+      name: "",
+    };
+
+    params.widgetService
+      .updateWidget(params.wgid, updateWidget)
+      .subscribe((widget: Widget) => {
+        params.router.navigate([
+          "/user",
+          params.uid,
+          "website",
+          params.wid,
+          "page",
+          params.pid,
+          "widget",
+        ]);
+      });
+  }
+
+  delete() {
+    this.widgetService
+      .deleteWidget(this.wgid)
+      .subscribe((widgets: Widget[]) => {
+        this.router.navigate([
+          "/user",
+          this.uid,
+          "website",
+          this.wid,
+          "page",
+          this.pid,
+          "widget",
+        ]);
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
-import { HttpModule } from '@angular/http'
-import { QuillEditorModule } from 'ngx-quill-editor';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
+import { QuillEditorModule } from "ngx-quill-editor";
 
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./Components/User/login/login.component";
+import { RegisterComponent } from "./Components/User/register/register.component";
+import { ProfileComponent } from "./Components/User/profile/profile.component";
+import { PageNewComponent } from "./Components/Page/page-new/page-new.component";
+import { PageEditComponent } from "./Components/Page/page-edit/page-edit.component";
+import { PageListComponent } from "./Components/Page/page-list/page-list.component";
+import { WebsiteListComponent } from "./Components/Website/website-list/website-list.component";
+import { WebsiteNewComponent } from "./Components/Website/website-new/website-new.component";
+import { WebsiteEditComponent } from "./Components/Website/website-edit/website-edit.component";
+import { WidgetEditComponent } from "./Components/Widget/widget-edit/widget-edit.component";
+import { WidgetChooserComponent } from "./Components/Widget/widget-chooser/widget-chooser.component";
+import { WidgetListComponent } from "./Components/Widget/widget-list/widget-list.component";
+import { WidgetHeaderComponent } from "./Components/Widget/widget-edit/widget-header/widget-header.component";
+import { WidgetImageComponent } from "./Components/Widget/widget-edit/widget-image/widget-image.component";
+import { WidgetYoutubeComponent } from "./Components/Widget/widget-edit/widget-youtube/widget-youtube.component";
+import { HttpClientModule } from "@angular/common/http";
+import { Routing } from "./app.routing";
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './Components/User/login/login.component';
-import { RegisterComponent } from './Components/User/register/register.component';
-import { ProfileComponent } from './Components/User/profile/profile.component';
-import { PageNewComponent } from './Components/Page/page-new/page-new.component';
-import { PageEditComponent } from './Components/Page/page-edit/page-edit.component';
-import { PageListComponent } from './Components/Page/page-list/page-list.component';
-import { WebsiteListComponent } from './Components/Website/website-list/website-list.component';
-import { WebsiteNewComponent } from './Components/Website/website-new/website-new.component';
-import { WebsiteEditComponent } from './Components/Website/website-edit/website-edit.component';
-import { WidgetEditComponent } from './Components/Widget/widget-edit/widget-edit.component';
-import { WidgetChooserComponent } from './Components/Widget/widget-chooser/widget-chooser.component';
-import { WidgetListComponent } from './Components/Widget/widget-list/widget-list.component';
-import { WidgetHeaderComponent } from './Components/Widget/widget-edit/widget-header/widget-header.component';
-import { WidgetImageComponent } from './Components/Widget/widget-edit/widget-image/widget-image.component';
-import { WidgetYoutubeComponent } from './Components/Widget/widget-edit/widget-youtube/widget-youtube.component';
-
-import { Routing } from './app.routing';
-
-import { UserService } from './services/user.service.client';
-import { WebsiteService } from './services/website.service.client';
-import { PageService } from './services/page.service.client';
-import { WidgetService } from './services/widget.service.client';
-import { FlickrService } from './services/flickr.service.client'
-import { OmdbComponent } from './Components/omdb/omdb.component';
-import {SharedService} from './services/shared.service.client'
-import { FlickrImageSearchComponent } from './Components/Widget/widget-edit/widget-image/flickr-image-search/flickr-image-search.component';
-import {AuthGuard} from './services/auth-guard.service';
-import { WidgetHtmlComponent } from './Components/Widget/widget-edit/widget-html/widget-html.component';
-import { WidgetTextComponent } from './Components/Widget/widget-edit/widget-text/widget-text.component';
+import { UserService } from "./services/user.service.client";
+import { WebsiteService } from "./services/website.service.client";
+import { PageService } from "./services/page.service.client";
+import { WidgetService } from "./services/widget.service.client";
+import { FlickrService } from "./services/flickr.service.client";
+import { OmdbComponent } from "./Components/omdb/omdb.component";
+import { SharedService } from "./services/shared.service.client";
+import { FlickrImageSearchComponent } from "./Components/Widget/widget-edit/widget-image/flickr-image-search/flickr-image-search.component";
+import { AuthGuard } from "./services/auth-guard.service";
+import { WidgetHtmlComponent } from "./Components/Widget/widget-edit/widget-html/widget-html.component";
+import { WidgetTextComponent } from "./Components/Widget/widget-edit/widget-text/widget-text.component";
 
 @NgModule({
   declarations: [
@@ -57,23 +56,26 @@ import { WidgetTextComponent } from './Components/Widget/widget-edit/widget-text
     OmdbComponent,
     FlickrImageSearchComponent,
     WidgetHtmlComponent,
-    WidgetTextComponent
+    WidgetTextComponent,
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     Routing,
     FormsModule,
     HttpModule,
-    QuillEditorModule
+    QuillEditorModule,
   ],
-  providers: [UserService, 
-  WebsiteService, 
-  PageService, 
-  WidgetService, 
-  FlickrService, 
-  SharedService,
-  AuthGuard],
-  
-  bootstrap: [AppComponent]
+  providers: [
+    UserService,
+    WebsiteService,
+    PageService,
+    WidgetService,
+    FlickrService,
+    SharedService,
+    AuthGuard,
+  ],
+
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
