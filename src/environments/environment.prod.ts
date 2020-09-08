9d57b33c6ee6472711fdb9f382a2dd11b80bf4d0
itsdayo <oladayogn@gmail.com>
2020-09-08 14:48:56 -0700

    amazon web service and toggling image button

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  accessKeyId: "AKIASF6DDRF634TMW4P3",
-  secretAccessKey: "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",
   production: true,
   baseUrl: "",
+  accessKeyId: "AKIASF6DDRF634TMW4P3",
+  secretAccessKey: "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",
 };
