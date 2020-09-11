0a948dfb9c327ba08f4c22c54ebffaa1d9ad7613
itsdayo <oladayogn@gmail.com>
2020-09-10 23:21:54 -0700

    updating secret keys

--- server/services/page.service.server.js
+++ server/services/page.service.server.js
@@ -1,73 +1,55 @@
-module.exports = function(app){
-  const pageModel = require('../models/page/page.model.server.js')
+module.exports = function (app) {
+  const pageModel = require("../models/page/page.model.server.js");
 
-app.post('/api/website/:wid/page',createPage);
-app.get('/api/website/:wid/page',findAllPagesForWebsite);
-app.get('/api/page/:pid',findPageById);
-app.put('/api/page/:pid',updatePage);
-app.delete('/api/page/:pid',deletePage);
-
+  app.post("/api/website/:wid/page", createPage);
+  app.get("/api/website/:wid/page", findAllPagesForWebsite);
+  app.get("/api/page/:pid", findPageById);
+  app.put("/api/page/:pid", updatePage);
+  app.delete("/api/page/:pid", deletePage);
 
   function createPage(req, res) {
-   var wid = req.params['wid']
+    var wid = req.params["wid"];
     var page = req.body;
-   // page._id = Math.floor(Math.random()*Math.floor(10000)).toString(); 
-   //   page.websiteId = websiteId;
-   //  pages.push(page);
+    // page._id = Math.floor(Math.random()*Math.floor(10000)).toString();
+    //   page.websiteId = websiteId;
+    //  pages.push(page);
 
-   pageModel.createPage(page).then(
-  page =>{
-  res.json( page);
-
-}
-    )
-    
+    pageModel.createPage(page).then((page) => {
+      res.json(page);
+    });
   }
 
-  function findAllPagesForWebsite(req,res) {
-   var wid = req.params['wid']
+  function findAllPagesForWebsite(req, res) {
+    var wid = req.params["wid"];
     // var result = [];
     // for (let x = 0; x < pages.length; x++) {
     //   if (pages[x].websiteId === websiteId) {
     //    result.push(pages[x]); }
 
-pageModel.findAllPagesForWebsite(wid).then(
-pages =>{
+    pageModel.findAllPagesForWebsite(wid).then((pages) => {
+      res.json(pages);
+    });
+  }
 
-  res.json(pages);
-}
-  )
-}
-
-  function findPageById(req,res) {
-    var pid = req.params['pid']
-   pageModel.findPageById(pid).then(
-data =>{
-    res.json(data)
-   
-}
-    )
-      }
- 
- function  updatePage(req, res) {
- let pid = req.params['pid'];
- let page = req.body;
-   pageModel.updatePage(pid,page).then(
-
-    data =>{
-       res.json(data)
-    })
-   
-}
-
-
- function deletePage(req, res) {
-  let pid = req.params['pid']
-   pageModel.deletePage(pid).then(
-    data =>{
+  function findPageById(req, res) {
+    var pid = req.params["pid"];
+    pageModel.findPageById(pid).then((data) => {
       res.json(data);
-    })
-      
+    });
+  }
 
-    }
-}
+  function updatePage(req, res) {
+    let pid = req.params["pid"];
+    let page = req.body;
+    pageModel.updatePage(pid, page).then((data) => {
+      res.json(data);
+    });
+  }
+
+  function deletePage(req, res) {
+    let pid = req.params["pid"];
+    pageModel.deletePage(pid).then((data) => {
+      res.json(data);
+    });
+  }
+};
