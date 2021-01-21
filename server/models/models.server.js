7c745a0d7e08450cbab7227d903c584339efb736
Oladayo Ogundipe <oladayoogundipe@oladayos-mbp.home>
2021-01-21 04:13:18 -0800

    setting up mongobd atlas"

--- server/models/models.server.js
+++ server/models/models.server.js
@@ -1,10 +1,11 @@
-var connectionString = 'mongodb://127.0.0.1:27017/web-dev'; // for local
+var connectionString = "mongodb://127.0.0.1:27017/web-dev"; // for local
 
-if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+if (process.env.MLAB_USERNAME_WEBDEV) {
+  // check if running remotely
   var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
   var password = process.env.MLAB_PASSWORD_WEBDEV;
-  connectionString = 'mongodb://' + username + ':' + password;
-  connectionString += '@ds121321.mlab.com:21321/heroku_pn1q8xh2'; // use yours
+  connectionString = "mongodb://" + username + ":" + password;
+  connectionString += "@cluster-nhg7v7rs.5fs4a/heroku_pn1q8xh2"; // use yours
 }
 
 var mongoose = require("mongoose");
