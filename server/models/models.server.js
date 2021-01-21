33767b2270f685166515b289388bfa70eb59d30a
Oladayo Ogundipe <oladayoogundipe@oladayos-mbp.home>
2021-01-21 04:59:54 -0800

    experimenting with atlas connection" "

--- server/models/models.server.js
+++ server/models/models.server.js
@@ -1,12 +1,14 @@
-var connectionString = "mongodb://127.0.0.1:27017/web-dev"; // for local
+//var connectionString = "mongodb://127.0.0.1:27017/web-dev"; // for local
 
-if (process.env.MLAB_USERNAME_WEBDEV) {
-  // check if running remotely
-  var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
-  var password = process.env.MLAB_PASSWORD_WEBDEV;
-  connectionString = "mongodb+srv://" + username + ":" + password;
-  connectionString += "@cluster-nhg7v7rs.5fs4a.mongodb.net/heroku_pn1q8xh2"; // use yours
-}
+// if (process.env.MLAB_USERNAME_WEBDEV) {
+// check if running remotely
+var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+var password = process.env.MLAB_PASSWORD_WEBDEV;
+// connectionString = "mongodb+srv://" + username + ":" + password;
+// connectionString += "@cluster-nhg7v7rs.5fs4a.mongodb.net/heroku_pn1q8xh2"; // use yours
+
+// }
+var connectionString = process.env.MONGO_CONNECTION;
 
 var mongoose = require("mongoose");
 var db = mongoose.connect(connectionString);
